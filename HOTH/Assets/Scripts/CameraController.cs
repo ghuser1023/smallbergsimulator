96af6f97999f698d8b851ea86644dbef451b5bed
Andy Peng <andypg20@gmail.com>
2018-11-17 21:17:20 -0800

    collisions

--- HOTH/Assets/Scripts/CameraController.cs
+++ HOTH/Assets/Scripts/CameraController.cs
@@ -18,4 +18,7 @@ public class CameraController : MonoBehaviour
     {
         transform.position = player.transform.position + offset;
     }
+
+
+
 }
--- HOTH/Assets/Scripts/SmallbergController.cs
+++ HOTH/Assets/Scripts/SmallbergController.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SmallbergController : MonoBehaviour {
+    private Rigidbody2D rb2d;
+    private float angerstart = -20;
     public float speed;
     Animator anim;
 
     // Use this for initialization
     void Start () {
         anim = GetComponent<Animator>();
-	}
+        rb2d = GetComponent<Rigidbody2D>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown("space")) {
+            anim.SetBool("angry", true);
+            angerstart = Time.time;
+        }
+        if (Time.time - 1.5 > angerstart) {
+            anim.SetBool("angry", false);
+        }
 	}
 
     private void FixedUpdate () {
