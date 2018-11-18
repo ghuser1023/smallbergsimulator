69df41f5f52a514cccbd84bc62f5fd5810d627bb
Andy Peng <andypg20@gmail.com>
2018-11-17 19:44:11 -0800

    smallberg moves!!! (left)

---
+++ HOTH/Assets/Scripts/SmallbergController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallbergController : MonoBehaviour {
+    public float speed;
+    Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        anim.SetFloat("dir", moveHorizontal);
+        transform.Translate((new Vector3(moveHorizontal, moveVertical, 0)) * speed);
+    }
+}
